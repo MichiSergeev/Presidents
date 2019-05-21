2e4afd50e8aa7f6b97d3759fcf530be58ad9a5a6
Michael <michael.sergeew@gmail.com>
2019-05-21 23:29:51 +0400

    the end

--- Presidents/DetailViewController.swift
+++ Presidents/DetailViewController.swift
@@ -3,30 +3,84 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var webView:UIWebView!
+    
+    private var languageListController: LanguageListControllerTableViewController?
+    private var languageButton: UIBarButtonItem?
+    
+    var languageString = "" {
+        didSet {
+            if languageString != oldValue {
+                configureView()
+            }
+        }
+    }
+    
+    private func modifyUrlForLanguage(url:String, language lang:String?) -> String {
+        var newUrl=url
+        if let langStr=lang {
+            let range=NSMakeRange(8, 2)
+            if !langStr.isEmpty && (url as NSString).substring(with: range) != langStr {
+                newUrl=(url as NSString).replacingCharacters(in: range, with: langStr)
+            }
+        }
+        return newUrl
+    }
 
 
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
             if let label = detailDescriptionLabel {
-                label.text = detail.description
+//                label.text = detail.description
+                let dict = detail as! [String:String]
+//                let urlString=dict["url"]!
+                let urlString=modifyUrlForLanguage(url: dict["url"]!, language: languageString)
+                label.text=urlString
+                
+                let url=NSURL(string: urlString)!
+                let request=URLRequest(url: url as URL)
+                webView.loadRequest(request)
+                let name=dict["name"]!
+                title=name
             }
         }
     }
 
+    @objc func showLanguagePopover() {
+        if languageListController==nil {
+            languageListController=LanguageListControllerTableViewController()
+            languageListController!.detailViewController=self
+            languageListController!.modalPresentationStyle = .popover
+        }
+        present(languageListController!, animated: true, completion: nil)
+        if let ppc=languageListController?.popoverPresentationController {
+            ppc.barButtonItem=languageButton
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         configureView()
+        languageButton=UIBarButtonItem(title: "Выбери язык", style: .plain, target: self, action: #selector(DetailViewController.showLanguagePopover))
+        navigationItem.rightBarButtonItem=languageButton
     }
-
-    var detailItem: NSDate? {
+    
+    
+    
+    
+    
+    
+    var detailItem: AnyObject? {
         didSet {
-            // Update the view.
             configureView()
         }
     }
 
 
+    
+    
+    
 }
 
---
+++ Presidents/LanguageListControllerTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  LanguageListControllerTableViewController.swift
+//  Presidents
+//
+//  Created by Mikhail Sergeev on 19/05/2019.
+//  Copyright © 2019 Mikhail Sergeev. All rights reserved.
+//
+
+import UIKit
+
+class LanguageListControllerTableViewController: UITableViewController {
+    
+    weak var detailViewController: DetailViewController?=nil
+    private let languageNames: [String] = ["English", "French", "German", "Spanish"]
+    private let languageCodes:[String]=["en", "fr", "de", "es"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        clearsSelectionOnViewWillAppear=false
+        preferredContentSize=CGSize(width: 320, height: (self.languageCodes.count*44))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return languageCodes.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // Configure the cell...
+        
+        cell.textLabel!.text=languageNames[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        detailViewController?.languageString=languageCodes[indexPath.row]
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
